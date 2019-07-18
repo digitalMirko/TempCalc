e85d2230ef0c06c6f392983481a345bc07a960f1
Mirko Cukich <mirkocukich@gmail.com>
2019-07-17 22:25:47 -0500

    IBOutlet and IBAction connections made on ViewController from Storyboard

--- Temp Calc/ViewController.swift
+++ Temp Calc/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Text field user fills in number
+    @IBOutlet weak var fillinTextField: UITextField!
+    // Label above textField where user enters number
+    // used to 'Enter Fahrenheit' or 'Enter Celsius' notice to user
+    @IBOutlet weak var enterLabel: UILabel!
+    // Thermometer graphic
+    @IBOutlet weak var graphicImageView: UIImageView!
+    // Calculation Output Label, changes depending on type selection
+    @IBOutlet weak var calcOutputLabel: UILabel!
+    // Segmented controller on the bottom
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    
+    // Action for the textview
+    @IBAction func calculate(_ sender: Any) {
+        
+        
+    }
+    
+    // Action for the segmented control to chamge between two calculations
+    @IBAction func formatCalculation(_ sender: Any) {
+        
+        
+    }
+    
 
 }
 
